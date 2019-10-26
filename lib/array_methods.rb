b9e18a1a22b73925ef388546a96ff09bf555c5d7
Ethan <ethan_gustafson@outlook.com>
2019-10-26 18:16:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def using_include(array, element)
+  array.include?(element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
     element1 = "wow"
     element2 = "Tardis"
-    array.include
 end
 
 def using_sort(array)
